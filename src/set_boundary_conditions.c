6d72c1c532bb88fa24a85c698911c18453d3f5e9
Julian Toumey <jtoumey@gmail.com>
2016-10-01 20:23:04 -0500

    Debugged an infinite loop in set_boundary_condition. I was not properly incrementing the counter in the south boundary for loop

--- src/set_boundary_conditions.c
+++ src/set_boundary_conditions.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "struct_list.h"
 // Calculate the maximum of two inputs (C does not include this natively)
 #define max(X,Y) (((X) > (Y)) ? (X) : (Y))
@@ -9,6 +10,7 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
    // Adjust coefficients for West boundary, running S to N
    for(ii = 0; ii < grid_data->ny; ii++)
    {
+      printf("\nWest boundary index: %d\n", ii);
       fvm_coeff[ii].a_W = 0.0;
       fvm_coeff[ii].a_E = phys_prop->Difx * phys_prop->Ae - max(0, -phys_prop->Fx * phys_prop->Ae); 
       fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->Aw * grid_data->phi_A + grid_data->phi_A * max(0, phys_prop->Fx * phys_prop->Aw);
@@ -18,8 +20,9 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
 
    // Adjust coefficients for South boundary, running W to E
    kk = grid_data->np - grid_data->ny + 1;
-   for(ii = 0; ii < kk; ii+grid_data->ny)
+   for(ii = 0; ii < kk; ii += grid_data->ny)
    {
+      printf("\nSouth bndry index: %d\n", ii);
       fvm_coeff[ii].a_S = 0.0;
       fvm_coeff[ii].a_N = phys_prop->Dify * phys_prop->An - max(0, -phys_prop->Fy * phys_prop->An); 
       fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->As * grid_data->phi_C + grid_data->phi_C * max(0, phys_prop->Fy * phys_prop->As);
@@ -29,8 +32,9 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
    }
 
    // Adjust coefficients for North boundary, running W to E
-   for(ii = grid_data->ny; ii < grid_data->np; ii+grid_data->ny)
+   for(ii = grid_data->ny - 1; ii < grid_data->np; ii += grid_data->ny)
    {
+      printf("\nNorth bndry index: %d\n", ii);
       fvm_coeff[ii].a_S = phys_prop->Dify * phys_prop->As - max(0, -phys_prop->Fy * phys_prop->As);
       fvm_coeff[ii].a_N = 0.0;
       fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->An * grid_data->phi_D - grid_data->phi_D * max(0, -phys_prop->Fy * phys_prop->An);
@@ -38,9 +42,11 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
 
    }
 
-   // Adjust coefficients for West boundary, running S to N
+   // Adjust coefficients for East boundary, running S to N
+   kk = kk - 1;
    for(ii = kk; ii < grid_data->np; ii++)
    {
+      printf("\nEast bndry index: %d\n", ii);
       fvm_coeff[ii].a_W = phys_prop->Difx * phys_prop->Aw - max(0, phys_prop->Fx * phys_prop->Aw);
       fvm_coeff[ii].a_E = 0.0; 
       fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->Ae * grid_data->phi_B - grid_data->phi_B * max(0, -phys_prop->Fx * phys_prop->Ae);
