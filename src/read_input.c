f755dc1118513e5a367038bde8d004f77bc79e12
Julian Toumey <jtoumey@gmail.com>
2016-10-04 20:17:53 -0500

    Updated logic in read_input

--- src/read_input.c
+++ src/read_input.c
@@ -15,79 +15,77 @@ void read_input(struct block *grid_data, struct properties *phys_prop)
       printf("*** Error opening the input file. ***\n");
    }
    // The input file is fixed format, so move through line-by-line
-   else
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
    {
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %d", &grid_data->nx);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %d", &grid_data->ny);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->xmin);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->xmax);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->ymin);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->ymax);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->phi_A);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->phi_B);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->phi_C);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &grid_data->phi_D);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &phys_prop->rho);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &phys_prop->gamma);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &phys_prop->u);
-      }
-      fgets(buffer, 100, fptr_input);
-      if(buffer != NULL)
-      {
-         sscanf(buffer, "%*s %f", &phys_prop->v);
-      }
-   }   
+      sscanf(buffer, "%*s %d", &grid_data->nx);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %d", &grid_data->ny);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->xmin);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->xmax);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->ymin);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->ymax);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->phi_A);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->phi_B);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->phi_C);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &grid_data->phi_D);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &phys_prop->rho);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &phys_prop->gamma);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &phys_prop->u);
+   }
+   fgets(buffer, 100, fptr_input);
+   if(buffer != NULL)
+   {
+      sscanf(buffer, "%*s %f", &phys_prop->v);
+   }
+   printf("*** Successfully read input file. ***\n");
 
    fclose(fptr_input);
 }
