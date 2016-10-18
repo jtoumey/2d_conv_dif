779c8148ea41c41fb0e3044a2aefefd6d84ba280
Julian Toumey <jtoumey@gmail.com>
2016-10-17 23:27:52 -0500

    Initial routine for checking which line is in buffer

--- src/read_input.c
+++ src/read_input.c
@@ -2,11 +2,15 @@
 #include "struct_list.h"
 #include <string.h>
 
+#define BUFFER_SIZE 100
+
 void read_input(struct block *grid_data, struct properties *phys_prop, struct settings *solv_settings)
 {
 
    FILE *fptr_input;
-   char buffer[100];
+   char buffer[BUFFER_SIZE];
+   char var_name[BUFFER_SIZE];
+   char *buffer_ptr_reset;
 
    // Open the input file
    fptr_input = fopen("input.inp", "r");
@@ -16,7 +20,39 @@ void read_input(struct block *grid_data, struct properties *phys_prop, struct se
       printf("*** Error opening the input file. ***\n");
    }
 
-   // The input file is fixed format, so move through line-by-line
+   while(fgets(buffer, BUFFER_SIZE, fptr_input))
+   {
+      buffer_ptr_reset = buffer;
+      sscanf(buffer, "%s%*[^\n]", var_name);
+      
+      printf("buffer is: %s\n", var_name);
+
+      if( !strncmp(var_name, "nx", BUFFER_SIZE))
+      {
+         printf("   buffer matches nx!\n");
+         sscanf(buffer_ptr_reset, "%*s %d", &grid_data->nx);
+
+
+         printf("the number of elements in x-dir: %i\n", grid_data->nx);
+      }
+      else if( !strncmp(var_name, "ny", BUFFER_SIZE))
+      {
+         printf("   buffer matches ny!\n");
+         sscanf(buffer_ptr_reset, "%*s %d", &grid_data->ny);
+
+         printf("the number of elements in y-dir: %i\n", grid_data->ny);
+      }
+      else
+      {
+         printf("no match :(\n");
+      }
+
+
+
+   }
+
+/*
+
    fgets(buffer, 100, fptr_input);
    if(buffer != NULL)
    {
@@ -92,6 +128,7 @@ void read_input(struct block *grid_data, struct properties *phys_prop, struct se
    {
       sscanf(buffer, "%*s %f", &solv_settings->tol);
    }
+*/
    printf("*** Successfully read input file. ***\n");
 
    fclose(fptr_input);
