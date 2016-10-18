5ef80f518688d982ce71b1fab627c2e7825397a1
Julian Toumey <jtoumey@gmail.com>
2016-10-17 23:48:25 -0500

    Added the full routine for interpreting parameters. This appears to work -- echoing the inputs as the code reads them matches the shuffled input file. Also, the result using the shuffled input file are identical to those from the regular input file.

--- src/read_input.c
+++ src/read_input.c
@@ -13,7 +13,7 @@ void read_input(struct block *grid_data, struct properties *phys_prop, struct se
    char *buffer_ptr_reset;
 
    // Open the input file
-   fptr_input = fopen("input.inp", "r");
+   fptr_input = fopen("input_alt.inp", "r");
 
    if(fptr_input == NULL)
    { 
@@ -42,93 +42,102 @@ void read_input(struct block *grid_data, struct properties *phys_prop, struct se
 
          printf("the number of elements in y-dir: %i\n", grid_data->ny);
       }
+      else if( !strncmp(var_name, "xmin", BUFFER_SIZE))
+      {
+         printf("   buffer matches xmin!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->xmin);
+
+         printf("the min in x: %f\n", grid_data->xmin);
+      }
+      else if( !strncmp(var_name, "xmax", BUFFER_SIZE))
+      {
+         printf("   buffer matches xmax!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->xmax);
+
+         printf("the max in x: %f\n", grid_data->xmax);
+      }
+      else if( !strncmp(var_name, "ymin", BUFFER_SIZE))
+      {
+         printf("   buffer matches ymin!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->ymin);
+
+         printf("the min in y: %f\n", grid_data->ymin);
+      }
+      else if( !strncmp(var_name, "ymax", BUFFER_SIZE))
+      {
+         printf("   buffer matches ymax!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->ymax);
+
+         printf("the max in y: %f\n", grid_data->ymax);
+      }
+      else if( !strncmp(var_name, "phi_A", BUFFER_SIZE))
+      {
+         printf("   buffer matches phi_A!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->phi_A);
+
+         printf("the BC value at A: %f\n", grid_data->phi_A);
+      }
+      else if( !strncmp(var_name, "phi_B", BUFFER_SIZE))
+      {
+         printf("   buffer matches phi_B!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->phi_B);
+
+         printf("the BC value at B: %f\n", grid_data->phi_B);
+      }
+      else if( !strncmp(var_name, "phi_C", BUFFER_SIZE))
+      {
+         printf("   buffer matches phi_C!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->phi_C);
+
+         printf("the BC value at C: %f\n", grid_data->phi_C);
+      }
+      else if( !strncmp(var_name, "phi_D", BUFFER_SIZE))
+      {
+         printf("   buffer matches phi_D!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &grid_data->phi_D);
+
+         printf("the BC value at D: %f\n", grid_data->phi_D);
+      }
+      else if( !strncmp(var_name, "rho", BUFFER_SIZE))
+      {
+         printf("   buffer matches rho!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &phys_prop->rho);
+
+         printf("the density rho: %f\n", phys_prop->rho);
+      }
+      else if( !strncmp(var_name, "gamma", BUFFER_SIZE))
+      {
+         printf("   buffer matches gamma!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &phys_prop->gamma);
+
+         printf("the diffusion coeff gamma: %f\n", phys_prop->gamma);
+      }
+      else if( !strncmp(var_name, "u", BUFFER_SIZE))
+      {
+         printf("   buffer matches u vel!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &phys_prop->u);
+
+         printf("the x-dir velocity: %f\n", phys_prop->u);
+      }
+      else if( !strncmp(var_name, "v", BUFFER_SIZE))
+      {
+         printf("   buffer matches v vel!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &phys_prop->v);
+
+         printf("the v-dir velocity: %f\n", phys_prop->v);
+      }
+      else if( !strncmp(var_name, "tol", BUFFER_SIZE))
+      {
+         printf("   buffer matches tolerance!\n");
+         sscanf(buffer_ptr_reset, "%*s %f", &solv_settings->tol);
+
+         printf("the tolerance: %f\n", solv_settings->tol);
+      }
       else
       {
          printf("no match :(\n");
       }
-
-
-
    }
-
-/*
-
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %d", &grid_data->nx);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %d", &grid_data->ny);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->xmin);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->xmax);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->ymin);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->ymax);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->phi_A);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->phi_B);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->phi_C);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &grid_data->phi_D);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &phys_prop->rho);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &phys_prop->gamma);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &phys_prop->u);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &phys_prop->v);
-   }
-   fgets(buffer, 100, fptr_input);
-   if(buffer != NULL)
-   {
-      sscanf(buffer, "%*s %f", &solv_settings->tol);
-   }
-*/
    printf("*** Successfully read input file. ***\n");
 
    fclose(fptr_input);
