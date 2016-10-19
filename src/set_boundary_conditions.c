2fe519992f65d7520f71db580890fa664479657f
Julian Toumey <jtoumey@gmail.com>
2016-10-18 22:03:21 -0500

    Updated cell face area var names in set_boundary_conditions.c

--- src/set_boundary_conditions.c
+++ src/set_boundary_conditions.c
@@ -7,14 +7,17 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
 {
    int ii, kk;
 
+   float a_w_conv, a_e_conv, a_s_conv, a_n_conv;
+   float a_w_diff, a_e_diff, a_s_diff, a_n_diff;
+
    // Adjust coefficients for West boundary, running S to N
    for(ii = 0; ii < grid_data->ny; ii++)
    {
 //      printf("\nWest boundary index: %d\n", ii);
       fvm_coeff[ii].a_W = 0.0;
-      fvm_coeff[ii].a_E = phys_prop->Difx * phys_prop->Ae + max(0, -phys_prop->Fx * phys_prop->Ae); 
-      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->Aw * grid_data->phi_A + grid_data->phi_A * max(0, phys_prop->Fx * phys_prop->Aw);
-      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Difx*phys_prop->Aw + max(0, phys_prop->Fx * phys_prop->Ae));
+      fvm_coeff[ii].a_E = phys_prop->Difx * phys_prop->area_east + max(0, -phys_prop->Fx * phys_prop->area_east); 
+      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->area_west * grid_data->phi_A + grid_data->phi_A * max(0, phys_prop->Fx * phys_prop->area_west);
+      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Difx*phys_prop->area_west + max(0, phys_prop->Fx * phys_prop->area_east));
 
    }
 
@@ -24,9 +27,9 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
    {
  //     printf("\nSouth bndry index: %d\n", ii);
       fvm_coeff[ii].a_S = 0.0;
-      fvm_coeff[ii].a_N = phys_prop->Dify * phys_prop->An + max(0, -phys_prop->Fy * phys_prop->An); 
-      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->As * grid_data->phi_C + grid_data->phi_C * max(0, phys_prop->Fy * phys_prop->As);
-      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Dify*phys_prop->As + max(0, phys_prop->Fy * phys_prop->As));
+      fvm_coeff[ii].a_N = phys_prop->Dify * phys_prop->area_north + max(0, -phys_prop->Fy * phys_prop->area_north); 
+      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->area_south * grid_data->phi_C + grid_data->phi_C * max(0, phys_prop->Fy * phys_prop->area_south);
+      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Dify*phys_prop->area_south + max(0, phys_prop->Fy * phys_prop->area_south));
  
 
    }
@@ -35,10 +38,10 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
    for(ii = grid_data->ny - 1; ii < grid_data->np; ii += grid_data->ny)
    {
   //    printf("\nNorth bndry index: %d\n", ii);
-      fvm_coeff[ii].a_S = phys_prop->Dify * phys_prop->As + max(0, phys_prop->Fy * phys_prop->As);
+      fvm_coeff[ii].a_S = phys_prop->Dify * phys_prop->area_south + max(0, phys_prop->Fy * phys_prop->area_south);
       fvm_coeff[ii].a_N = 0.0;
-      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->An * grid_data->phi_D + grid_data->phi_D * max(0, -phys_prop->Fy * phys_prop->An);
-      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Dify*phys_prop->An + max(0, -phys_prop->Fy * phys_prop->As));
+      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Dify * phys_prop->area_north * grid_data->phi_D + grid_data->phi_D * max(0, -phys_prop->Fy * phys_prop->area_north);
+      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Dify*phys_prop->area_north + max(0, -phys_prop->Fy * phys_prop->area_south));
 
    }
 
@@ -47,10 +50,10 @@ void set_boundary_conditions(struct block *grid_data, struct coeff *fvm_coeff, s
    for(ii = kk; ii < grid_data->np; ii++)
    {
    //   printf("\nEast bndry index: %d\n", ii);
-      fvm_coeff[ii].a_W = phys_prop->Difx * phys_prop->Aw + max(0, phys_prop->Fx * phys_prop->Aw);
+      fvm_coeff[ii].a_W = phys_prop->Difx * phys_prop->area_west + max(0, phys_prop->Fx * phys_prop->area_west);
       fvm_coeff[ii].a_E = 0.0; 
-      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->Ae * grid_data->phi_B + grid_data->phi_B * max(0, -phys_prop->Fx * phys_prop->Ae);
-      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Difx*phys_prop->Ae + max(0, -phys_prop->Fx * phys_prop->Aw));
+      fvm_coeff[ii].S_u = fvm_coeff[ii].S_u + 2*phys_prop->Difx * phys_prop->area_east * grid_data->phi_B + grid_data->phi_B * max(0, -phys_prop->Fx * phys_prop->area_east);
+      fvm_coeff[ii].S_p = fvm_coeff[ii].S_p + -(2*phys_prop->Difx*phys_prop->area_east + max(0, -phys_prop->Fx * phys_prop->area_west));
 
    }
 
